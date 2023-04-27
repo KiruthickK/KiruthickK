bb65aa91f41ee773b5cb52b7c5c000703444520c
KiruthickK <kiruthick101@gmail.com>
2023-04-28 00:15:31 +0530

    Construction sign added

--- scripts.js
+++ scripts.js
@@ -1,19 +1,23 @@
 function GitDisplay() {
+    hheeight();
     document.getElementById("logo").src = "Images/git-logo.png"
     document.getElementById("logo").style.visibility = 'visible';
 }
 
 function GitDisplayBack() {
     document.getElementById("logo").style.visibility = 'hidden';
+    contruction();
 }
 
 function HackerRankDisplay() {
+    hheeight();
     document.getElementById("logo").src = "Images/Hackerrank-logo.png"
     document.getElementById("logo").style.visibility = 'visible';
 }
 
 function HackerRankDisplayBack() {
     document.getElementById("logo").style.visibility = 'hidden';
+    contruction();
 }
 
 function LeetcodeDisplay() {
@@ -27,4 +31,17 @@ function LeetcodeDisplayBack() {
     document.getElementById("logo").style.height = "250px";
     document.getElementById("logo").style.width = "250px";
     document.getElementById("logo").style.visibility = 'hidden';
+    contruction()
+}
+
+function hheeight() {
+    document.getElementById("logo").style.height = "250px";
+    document.getElementById("logo").style.width = "250px";
+}
+
+function contruction() {
+    document.getElementById("logo").style.visibility = 'visible';
+    // document.getElementById("logo").style.height = "400px";
+    // document.getElementById("logo").style.width = "400px";
+    document.getElementById("logo").src = "Images/construction.png";
 }
