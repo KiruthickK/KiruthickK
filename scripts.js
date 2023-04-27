62781d54fae8ef95a3a551fef31f217103fa6860
KiruthickK <kiruthick101@gmail.com>
2023-04-27 23:53:16 +0530

    Basics added

---
+++ scripts.js
@@ -0,0 +1,30 @@
+function GitDisplay() {
+    document.getElementById("logo").src = "Images/git-logo.png"
+    document.getElementById("logo").style.visibility = 'visible';
+}
+
+function GitDisplayBack() {
+    document.getElementById("logo").style.visibility = 'hidden';
+}
+
+function HackerRankDisplay() {
+    document.getElementById("logo").src = "Images/Hackerrank-logo.png"
+    document.getElementById("logo").style.visibility = 'visible';
+}
+
+function HackerRankDisplayBack() {
+    document.getElementById("logo").style.visibility = 'hidden';
+}
+
+function LeetcodeDisplay() {
+    document.getElementById("logo").src = "Images/leetcode-logo.png"
+    document.getElementById("logo").style.height = "150px";
+    document.getElementById("logo").style.width = "250px";
+    document.getElementById("logo").style.visibility = 'visible';
+}
+
+function LeetcodeDisplayBack() {
+    document.getElementById("logo").style.height = "250px";
+    document.getElementById("logo").style.width = "250px";
+    document.getElementById("logo").style.visibility = 'hidden';
+}
